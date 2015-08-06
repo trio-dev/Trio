d64bbe8f177ddc14226ec8a8967fd21850c98376
chikeichan <chikeichan@gmail.com>
2015-08-06 01:19:25 -0700

    Done with new API for renderer class

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -6,12 +6,20 @@ Renderer.prototype.createTemplate = function() {
     return new Template();
 };
 
-var Template = function(){
-    this._currentState = [];
-    this._queue = [];
-};
+var _currentState = [];
+var _queue = [];
+var _conditional = undefined;
+var _state;
+var _loop;
+var _start;
+var Template = function(){};
 
-Template.prototype.open = function(tagName){
+/**
+ * Create DOM node
+ * @param  {string} tagName Element name
+ * @return {instance}       this
+ */
+Template.prototype.create = function(tagName){
     tagName = parseTag(tagName);
     var fn = function() {
         var el = document.createElement(tagName[0]);
@@ -20,63 +28,158 @@ Template.prototype.open = function(tagName){
         } else if (tagName[1] === '#') {
             el.id = tagName[2];
         }
-        this._currentState.push(el);
+        _currentState.push(el);
     }.bind(this)
-    this._queue.push(fn);
+    _queue.push({
+        type: 'open',
+        fn: fn
+    });
     return this;
 };
 
 Template.prototype.addClass = function(className) {
-    var fn = function() {
+    var fn = function(d) {
         var el = grabLast.call(this);
+        className = evaluate(d, className);
         var separator = el.className.length > 0 ? ' ' : '';
         if (!hasClass(el,className)) {
             el.className += separator + className;
         }
     }.bind(this);
-    this._queue.push(fn);
+    _queue.push({
+        type: 'addClass',
+        fn: fn
+    });
+    return this;
+};
+
+Template.prototype.text = function(content) {
+    var fn = function(d) {
+        var el = grabLast.call(this);
+        el.textContent = content;
+    }.bind(this);
+    _queue.push({
+        type: 'text',
+        fn: fn
+    });
     return this;
 };
 
 Template.prototype.removeClass = function(className) {
-    var fn = function() {
+    var fn = function(d) {
         var el = grabLast.call(this);
+        className = evaluate(d, className);
         if (hasClass(el,className)) {
             var reg = new RegExp('(\\s|^)'+className+'(\\s|$)');
             el.className = el.className.replace(reg,' ');
         }
     }.bind(this);
-    this._queue.push(fn);
+    _queue.push({
+        type: 'removeClass',
+        fn: fn
+    });
     return this;
 };
 
-Template.prototype.close = function() {
+Template.prototype.append = function() {
     var fn = function(d) {
-        var el = this._currentState.pop();
-        if (this._currentState.length === 0) {
-            this.fragments.appendChild(el);
+        var el = _currentState.pop();
+        if (_currentState.length === 0) {
+            this.previousFragment.appendChild(el);
         } else {
-            grabLast.call(this).appendChild(el);
+            var parent = grabLast.call(this);
+            parent.appendChild(el);
         }
     }.bind(this);
-    this._queue.push(fn);
+    _queue.push({
+        type: 'close',
+        fn: fn
+    });
     return this;
 };
 
-Template.prototype.if = function(cb) {
+Template.prototype.if = function(funcOrKey) {
+    var fn = function(d) {
+        _state = 'conditional';
+        funcOrKey = evaluate(d, funcOrKey);
+        _conditional = !!funcOrKey;
+    }.bind(this)
+    _queue.push({
+        type: 'if',
+        fn: fn
+    });
+    return this;
+}
 
+Template.prototype.else = function() {
+    var fn = function(d) {
+        _conditional = !_conditional;
+    }.bind(this);
+    _queue.push({
+        type: 'else',
+        fn: fn
+    });
+    return this;
+}
+
+Template.prototype.each = function(funcOrKey) {
+    var fn = function(d, i) {
+        funcOrKey = evaluate(d, funcOrKey);
+        _loop  = funcOrKey;
+        _state = 'loop';
+        _start = i;
+    }.bind(this);
+    _queue.push({
+        type: 'each',
+        fn: fn
+    });
+    return this;
+}
+
+Template.prototype.done = function() {
+    var fn = function(d, i) {
+        _conditional = undefined;
+        _state       = undefined;
+    }.bind(this);
+    _queue.push({
+        type: 'done',
+        fn: fn
+    });
+    return this;
 }
 
 Template.prototype.render = function(data) {
-    this.fragments = document.createDocumentFragment();
-    this._queue.forEach(function(fn) {
-        fn(data);
+    this.previousFragment = document.createDocumentFragment();
+    _queue.forEach(function(q, i) {
+        switch (_state) {
+            case 'conditional':
+                if (_conditional || q.type === 'else' || q.type === 'done') {
+                    q.fn(data, i);
+                }
+                break;
+            case 'loop':
+                if (q.type === 'done') {
+                    _loop.forEach(function(l, j) {
+                        for (var start = _start + 1; start < i; start++) {
+                            var loopFn = _queue[start];
+                            loopFn.fn(l, j);
+                        }
+                    }.bind(this));
+                    q.fn(data, i);
+                }
+                break;
+            default:
+                q.fn(data, i);
+                break;
+                
+        }
     }.bind(this));
-    return this.fragments;
+
+    return this.previousFragment;
 };
 
 function grabLast() {
-    return this._currentState[this._currentState.length - 1];
+    return _currentState[_currentState.length - 1];
 };
 
 function hasClass(el, className) {
@@ -88,5 +191,21 @@ function parseTag(tag) {
              .split(',');
     return tag;
 };
+
+function evaluate(data, funcOrKey) {
+    switch (typeof funcOrKey) {
+        case 'function':
+            return funcOrKey.apply(this, arguments);
+            break;
+        case 'string':
+            var keys = funcOrKey.split('.');
+            var ans = data;
+            keys.forEach(function(key, i) {
+                ans = data[key];
+            });
+            return ans;
+            break;
+    }
+}
 
 module.exports = Renderer;
